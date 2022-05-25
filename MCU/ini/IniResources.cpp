97f489a25811c80b154f4d8b70fdeaf1b273f948
Gerasim Gerasimov <dialix@yandex.ru>
2022-05-26 00:17:31 +0700

    секции к прочтению организовал, цикл для их чтения тоже организовал, остаётся парсить строки в какие-то списки.

--- MCU/ini/IniResources.cpp
+++ MCU/ini/IniResources.cpp
@@ -1,6 +1,8 @@
 #include "IniResources.h"
 #include "parser.h"
 
+const std::vector<std::string> sections = { "[vars]", "[RAM]", "[FLASH]", "[CD]"};
+
 void IniResources::init(void) {
 	const std::vector<std::string> devices = {"U1"};
 	for (auto & device : devices) {
@@ -9,10 +11,12 @@ void IniResources::init(void) {
 			IniParser::setRoot(itemLimits.RootOffset, itemLimits.Size);
 			/*TODO осталось парсить
 			как обычно, найти секции RAM, FLASH, CD, vars*/
-			if (IniParser::setSectionToRead((char*)"[RAM]")) {
-				TSectionReadResult readResult = { NULL, 0 };
-				while ((readResult = IniParser::getNextTagString()), readResult.result != 0) {
-					//TODO парсить полученную строку используя её длину в tagSuccess и указатель на начало
+			for (auto& section : sections) {
+				if (IniParser::setSectionToRead((char*)section.c_str())) {
+					TSectionReadResult readResult = { NULL, 0 };
+					while ((readResult = IniParser::getNextTagString()), readResult.result != 0) {
+						//TODO парсить полученную строку используя её длину в tagSuccess и указатель на начало
+					}
 				}
 			}
 		}
