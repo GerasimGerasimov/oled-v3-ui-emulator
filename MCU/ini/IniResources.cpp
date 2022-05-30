c54b22e5c3d713620ac01e48e0a10fbf3b76096b
Gerasim Gerasimov <dialix@yandex.ru>
2022-05-30 11:48:17 +0700

    неудачная попытка избавится от квадратных скобок используя MAP {"vars","[vars]"}

--- MCU/ini/IniResources.cpp
+++ MCU/ini/IniResources.cpp
@@ -2,6 +2,13 @@
 #include "parser.h"
 
 const std::vector<std::string> sections = { "[vars]", "[RAM]", "[FLASH]", "[CD]"};
+
+const std::map<std::string, std::string> msections = { {"vars","[vars]"}, {"RAM","[RAM]"}, {"FLASH","[FLASH]"},{"CD","[CD]"} };
+/*TODO иде€ хороша€ применить ћј– чтобы не вычлен€ть квадратные скобки
+но € хочу сначала vars получить, а мне подунули CD.. может есть MAP который извлекает
+знечени€ в заданном пор€дке?*/
+
+
 std::map<std::string, std::map<std::string, std::vector<ISignal* >>> IniResources::devs = {};
 /*TODO обеспечить такую структуру
 U1
@@ -24,16 +31,16 @@ void IniResources::init(void) {
 			IniParser::setRoot(itemLimits.RootOffset, itemLimits.Size);
 			/*TODO осталось парсить
 			как обычно, найти секции RAM, FLASH, CD, vars*/
-			for (auto& section : sections) {
-				if (IniParser::setSectionToRead((char*)section.c_str())) {
+			for (auto& section : msections) {
+				if (IniParser::setSectionToRead((char*)section.second.c_str())) {
 					std::vector<ISignal*> params = {};
 					TSectionReadResult readResult = { NULL, 0 };
 					while ((readResult = IniParser::getNextTagString()), readResult.result != 0) {
 						//TODO парсить полученную строку использу€ еЄ длину в tagSuccess и указатель на начало
-						ISignal* s = TIniString::getSignal(section, readResult.tag, readResult.result);
+						ISignal* s = TIniString::getSignal(section.first, readResult.tag, readResult.result);
 						params.push_back(s);
 					}
-					devs[device][section] = params;
+					devs[device][section.first] = params;
 				}
 			}
 		}
