85539a8df568212627a4c6b0090d7c65332de8e5
Gerasim Gerasimov <dialix@yandex.ru>
2022-11-12 22:17:27 +0700

    1) в ModbusSlave реализован метод setValue(tag, value, callback) для записи значения параметра 2) благодаря п1, код PageEditValue значительно сократился

--- MCU/Modbus/ModbusSlave.h
+++ MCU/Modbus/ModbusSlave.h
@@ -1,12 +1,24 @@
 #include "crc16.h"
 #include "bastypes.h"
 #include <string>
+#include <map>
+#include <functional>
+#include <Slot.h>
+
+typedef struct {
+	const std::string& Cmd;
+	const std::string& DevAddr;
+	const std::string& RegAddr;
+	const std::string& Value;
+} TWriteCmdSrc;
 
 class ModbusSlave {
+	using TSlotDataHandler = std::function<void(Slot& slot, u8* reply)>;
 public:
-	static void getCmdWriteFloatReg();
-	static void getCmdWriteU16Reg(u16 reg, u16 value);
-	static u16 NumericRegAddrToU16(char* RegAddr);
-	static u16 BitRegAddrToU16(char* RegAddr);
-	static u16 ByteRegAddrToU16(char* RegAddr);
+	static void setValue(std::string& tag, std::string& value, TSlotDataHandler callback);
+private:
+	static u8 CreateWriteCmd(u8* a, TWriteCmdSrc CmdSrc);
+	static u8 get0x10WriteRegCmd(u8* a, TWriteCmdSrc& Src);
+	static std::string NetWorkAddrToHex(u16 nwa);
+	static const std::map < std::string, std::function <u8(u8*, TWriteCmdSrc&) >> WriteCmdVariants;
 };
--- MCU/Pages/EditValue/PageEditValue.cpp
+++ MCU/Pages/EditValue/PageEditValue.cpp
@@ -1,9 +1,8 @@
 ﻿#include "PageEditValue.h"
 #include "Router.h"
 #include <IniResources.h>
-#include <crc16.h>
 #include <Slot.h>
-#include <DevicePollManager.h>
+#include <ModbusSlave.h>
 
 void TPageEditValue::view() {
     MainMenu->view();
@@ -13,10 +12,10 @@ void TPageEditValue::clear() {
 }
 
 void TPageEditValue::onOpen() {
-    p = (TParameter*)IniResources::getSignalByTag(TRouter::PageValueEditEntryData.tag);
+    tag = TRouter::PageValueEditEntryData.tag;
+    p = (TParameter*)IniResources::getSignalByTag(tag);
     pHeader->setCaption(p->getName());
     pEdit->setCaption(TRouter::PageValueEditEntryData.value);
-    tag = TRouter::PageValueEditEntryData.tag;
 }
 
 bool TPageEditValue::ProcessMessage(TMessage* m) {
@@ -40,71 +39,6 @@ bool TPageEditValue::ProcessMessage(TMessage* m) {
     return false;
 };
 
-std::string NetWorkAddrToHex(u16 nwa) {
-    char s[4];
-    sprintf(s, "%.2X", nwa);
-    std::string res(s);
-    return res;
-}
-
-typedef struct {
-    const std::string& Cmd;
-    const std::string& DevAddr;
-    const std::string& RegAddr;
-    const std::string& Value;
-} TWriteCmdSrc;
-
-/*
-AD, C, №R(h, l), CW(h, l), CntBytes, Data[0](h, l)..Data[CW](h, l), CRC(h, l)
-│   │  │         │          │        │             │                └─(word)crc16
-│   │  │         │          │        └─────────────┴─(word)Записываемые данные
-│   │  │         │          └─(byte)Кол - во передаваемых байт
-│   │  │         └─(word)Кол - во регистров к передаче
-│   │  └─(word)Начальный адрес
-│   └────(byte)Команда(10h)
-└──────(byte)Адрес устройства
-*/
-
-u16 get0x10WriteRegCmd(u8* a, TWriteCmdSrc& Src) {
-    //u8 a[13];
-    a[0] = (u8)std::stoi(Src.DevAddr);
-    a[1] = 0x10;
-    //начальный адрес регистра
-    u16 addr = std::stoul(Src.RegAddr, nullptr, 16);
-    a[2] = (u8)(addr >> 8) & 0x00FF;
-    a[3] = (u8)(addr & 0x00FF);
-    //кол-во передаваемых регистров
-    u8 regscnt = Src.Value.size() / 4;
-    a[4] = 0;
-    a[5] = regscnt;
-    a[6] = regscnt * 2;//кол-во передаваемых байт
-    //данные
-    u32 value = std::stol(Src.Value, nullptr, 16);
-    (regscnt == 1)
-        ? ( a[7] = (value & 0x0000FF00) >> 8,
-            a[8] = (value & 0x000000FF) >> 0)
-        : ( a[7] = (value & 0xFF000000) >> 24,
-            a[8] = (value & 0x00FF0000) >> 16,
-            a[9] = (value & 0x0000FF00) >> 8,
-            a[10]= (value & 0x000000FF) >> 0);
-    //контрольная сумма 
-    u16 cmdLen = (regscnt == 1) ? 11 : 13;
-    FrameEndCrc16(a, cmdLen);
-    return cmdLen;
-}
-
-static const std::map < std::string, std::function <u16 (u8*, TWriteCmdSrc&) >> WriteCmdVariants = {
-    {"10", [](u8* a, TWriteCmdSrc& props) {return get0x10WriteRegCmd(a, props); }},
-    {"16", [](u8* a, TWriteCmdSrc& props) {return 0; }},
-};
-
-u16 CreateWriteCmd(u8* a, TWriteCmdSrc CmdSrc) {
-    u16 res = (WriteCmdVariants.count(CmdSrc.Cmd))
-        ? WriteCmdVariants.at(CmdSrc.Cmd)(a, CmdSrc)
-        : 0;
-    return res;
-}
-
 void SlotU1RAMUpdate(Slot& slot, u8* reply) {
     slot.Flags |= (u16)SlotStateFlags::SKIP_SLOT;
     //тут бы можно было из массива reply куда-то скопировать результат,
@@ -112,30 +46,8 @@ void SlotU1RAMUpdate(Slot& slot, u8* reply) {
 }
 
 void TPageEditValue::sendValue(void) {
-    u8 cmd[11];
     std::string value = pEdit->getValue();
-    std::string ValueHex = p->getValueHex(value);//получил значение в хексах и сразу длину (так как строка)
-    std::string RegHexAddr = p->getRegHexAddr();//получил номер регистра в хексах
-    /*getWriteCmdType узнаю какой командой отправлять данные
-        Write Multiple Registers - 0х10
-        Mask Write Register - 0x16 для манипуляций с битами и байтами*/
-    const std::string Cmd = p->getWriteCmdType();
-    /*по Tag надо узнаю на какой Адрес отправлять
-    "DEVICES":"U1/", есть список устройств, надо сопостоавить из тега U1
-        "U1":"DEV1/COM1/1 а из U1 найти адрес (он за COM1/)
-    */
-    const u16 DevAddr = IniResources::getDevNetWorkAddrByTag(tag);
-    const const std::string DevAddrHex = NetWorkAddrToHex(DevAddr);
-    /*TODO выяснить в какой командный слот писать полученную команду
-    по позиционному номеру U1 U2 надо выяснить к какому COM они подключены и выдать ссылку на соответсвующий слот*/
-    const std::string DevPos = IniResources::getDevicePositionByTag(tag);
-    const std::string Section = "CmdWrite";
-    Slot* slot = DevicePollManager::getSlotByDevPosAndSection(DevPos, Section);
-    /*TODO полученный массив положить в OUT командного слота*/
-    slot->cmdLen = CreateWriteCmd(slot->OutBuf, { Cmd, DevAddrHex, RegHexAddr, ValueHex });
-    slot->onData = &SlotU1RAMUpdate;
-    slot->Flags &= ~(u16)SlotStateFlags::SKIP_SLOT;
-    /*TODO активировать слот на передачу и показывать анимацию до завершения записи*/
+    ModbusSlave::setValue(tag, value, SlotU1RAMUpdate);
     /*TODO если запись успешна - показать анимаци успешной записаи, если нет соотв неуспешной*/
 }
 
