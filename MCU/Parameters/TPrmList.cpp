b8781203858701ba8f2f83b5c700630768824ef6
Gerasim Gerasimov <dialix@yandex.ru>
2022-12-24 23:26:34 +0700

    TPrmList парсинг /x01#9600/x02#19200/x03#57600/x04#115200/x04/ в первом приближении работает

--- MCU/Parameters/TPrmList.cpp
+++ MCU/Parameters/TPrmList.cpp
@@ -25,6 +25,8 @@ std::string TPrmList::getMSU()
 
 void TPrmList::setValue() {}
 
+//p501=RS485_1_BPS/Baudrate/TPrmList/xF001/r2000.H/BPS//x01#9600/x02#19200/x03#57600/x04#115200/x04/
+
 std::string TPrmList::getValue(const TSlotHandlerArsg& args, const char* format) {
 	std::string res = validation(args);
 	return (res != "")
@@ -34,8 +36,40 @@ std::string TPrmList::getValue(const TSlotHandlerArsg& args, const char* format)
 
 const std::string TPrmList::value(const TSlotHandlerArsg& args, const char* format) {
 	u8 input = getRawValue(args);
-	float res = input * Scale;
-	return Utils::getValueAsFormatStr(res, Utils::getFormat(res));
+	//найти 7-й слэш
+	// [7+0]            [7+1]                [7+N]
+	// /index0#data0#ai0/index1#data1#ai1/.../indexN#dataN#aiN/Base/
+	// Вase отличается тем, что в нём нет символа '#'
+	char* options = optional;//приходится сохранять указатели так как метод ниже меняет его
+	char* plist = IniParser::getElementPtrByNumber(4, '/', options);
+	char* listAddr = plist;//приходится сохранять указатели так как метод ниже меняет его
+	int size = IniParser::getStringLenght(&listAddr);
+	std::vector<std::string> list = IniParser::getListOfDelimitedString('/', plist, size);
+	//Список получил
+	//обрататываю строки вида x01#9600, надо выделить число до # (вдс x01 сравнить его с input)
+	//и если совпало, то выделяю строку после #, это и будет результат
+	char buf[10];
+	for (auto& e : list) {
+		u8 ssize = e.size();
+		for (int i = 0; i < ssize; i++) {
+			if (e[i] != '#') {
+				buf[i] = e[i];
+			}
+			else {//дошёл до # в buf должно накопится какое-то число
+				buf[i] = 0;//конец строки
+				u8 offset = (buf[0] == 'x') ? 1 : 0;
+				int idx = std::stoi(&buf[offset], 0, 16);
+				if (idx == input) {
+					std::string res = std::string(e, i+1);
+					return res;
+				}
+				break;
+			}
+		}
+	}
+	//float res = input * Scale;
+	//return Utils::getValueAsFormatStr(res, Utils::getFormat(res));
+	return "---";
 }
 
 u8 TPrmList::getRawValue(const TSlotHandlerArsg& args) {
--- MCU/ini/parser.h
+++ MCU/ini/parser.h
@@ -28,12 +28,12 @@ public:
 	static std::string getElement(char delimiter, char* ptr);
 	static char* getElementPtrByNumber(int number, char delimiter, char* src);
 	static int isDelimiterSizeLimited(char delimiter, char*& src, int& size);
+	static int getStringLenght(char** ptr);
 private:
 	static void resetFind(char* start);
 	static char* getSectionEntryPoint(char* SectionName);
 	static int getTagString(char** position);
 	static int isDelimiter(char** ptr, char Delimiter);
-	static int getStringLenght(char** ptr);
 	static int getSectionLinesCount(char* SectionName);
 	static std::string getElementInclude(char delimiter, char** ptr, int& size);
 	static char* Root;
