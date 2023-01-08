5fc9f26fad20f4b1b6a8885550e7d420395a2ca9
Gerasim Gerasimov <dialix@yandex.ru>
2023-01-08 23:07:46 +0700

    Дошёл до CmdSender::sendCmd

--- MCU/CmdSender/CmdSender.cpp
+++ MCU/CmdSender/CmdSender.cpp
@@ -1,13 +1,47 @@
 #include "CmdSender.h"
 #include <IniResources.h>
 
-u16 CmdSender::keyStopRun = 0;
-u16 CmdSender::keyMode = 0;
+u16 CmdSender::keyStopReset = 0;
+u16 CmdSender::keyRun = 0;
+u16 CmdSender::keyClean = 0;
+u16 CmdSender::keyVAC = 0;
+u16 CmdSender::keyPrevStopReset = 0;
+u16 CmdSender::keyPrevRun = 0;
+u16 CmdSender::keyPrevClean = 0;
+u16 CmdSender::keyPrevVAC = 0;
+bool CmdSender::cmdSendInProcess = false;
+u16 CmdSender::TryCnt = 3;
+
+static const u16 CMD_RUN = 1111;
+static const u16 CMD_STOP = 1000;
+static const u16 CMD_RESET = 2222;
+
+static const u16 CMD_NORMAL = 5310;
+static const u16 CMD_CLEAN = 5311;
+static const u16 CMD_VAC = 5312;
+static const u16 CMD_TEST = 5313;
 
 void CmdSender::init() {
 
 }
 
 void CmdSender::update(const u16 din) {
+	updateKeyRun(din);
+}
 
+void CmdSender::updateKeyRun(const u16 din) {
+	keyRun = (din & (1 << 1));
+	if (keyPrevRun != keyRun) {
+		if (keyRun) {
+			if (cmdSendInProcess != true) {
+				sendCmd(CMD_RUN);
+			}
+		}
+		keyPrevRun = keyRun;
+	}
+}
+
+void CmdSender::sendCmd(const u16 cmd) {
+	TryCnt = 3;
+	cmdSendInProcess = true;
 }
--- MCU/CmdSender/CmdSender.h
+++ MCU/CmdSender/CmdSender.h
@@ -10,8 +10,18 @@ class CmdSender {
 		static void init(void);
 		static void update(const u16 din);
 	private:
-		static u16 keyStopRun;
-		static u16 keyMode;
+		static u16 keyStopReset;
+		static u16 keyRun;
+		static u16 keyClean;
+		static u16 keyVAC;
+		static u16 keyPrevStopReset;
+		static u16 keyPrevRun;
+		static u16 keyPrevClean;
+		static u16 keyPrevVAC;
+		static bool cmdSendInProcess;
+		static u16 TryCnt;
+		static void updateKeyRun(const u16 din);
+		static void sendCmd(const u16 cmd);
 };
 
 #endif
