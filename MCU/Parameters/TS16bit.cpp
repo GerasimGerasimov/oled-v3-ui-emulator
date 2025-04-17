27627762879bed0112633c4f6e6cc04d9bd3915d
Loreleiyy <vadim-lorelei@yandex.ru>
2025-04-17 17:19:56 +0700

    Добавлена возможность записи в S16 переменные

--- MCU/Parameters/TS16bit.cpp
+++ MCU/Parameters/TS16bit.cpp
@@ -8,6 +8,15 @@ TS16BIT::TS16BIT(TSignalPropsPointers props)
 TS16BIT::~TS16BIT(){
 }
 
+const std::string TS16BIT::getValueHex(std::string& src){
+	s16 value = string2raw(src);
+	char s[5];
+	//GIST "%.4X" преобразование числа в hex с заданным кол-вом значащих нулей
+	sprintf(s, "%04X", static_cast<uint16_t>(value));
+	std::string res(s);
+	return res;
+}
+
 TGenaralCaseRawReturn TS16BIT::getRawValue(const TSlotHandlerArsg& args) {
 	s16 offset = Addr - args.StartAddrOffset;
 	u8* p = args.InputBuf + offset;//получил указатель на данные
@@ -18,6 +27,12 @@ TGenaralCaseRawReturn TS16BIT::getRawValue(const TSlotHandlerArsg& args) {
 	res.type = TGeneralCaseReturnType::S;
 	res.raw.s = raw.s;
 	return res;
+}
+
+s16 TS16BIT::string2raw(std::string& src){
+	float f = std::stof(src);
+	f /= Scale;
+	return (s16)f;
 }
 
 TInternalMemAddress TS16BIT::getInternalMemAddr() {
