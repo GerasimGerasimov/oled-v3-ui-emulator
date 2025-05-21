c793b8b62e4d355e1b70f5ff65883dc60203c890
Надежда Бурнос <N.Burnos@Intmash.Company>
2025-05-21 17:28:53 +0700

    Реализован выбор режима работы по кнопкам

--- MCU/Components/OperatingMode.cpp
+++ MCU/Components/OperatingMode.cpp
@@ -1,33 +1,103 @@
 #include "OperatingMode.h"
 #include "graphics.h"
+#include <iostream>
+#include <cmath>
 
-OperatingMode::OperatingMode(int x, int y) {
-	this->x = x;
-	this->y = y;
+OperatingMode::OperatingMode(int x, int y, u8 colorState) {
+	ElementRect.Left = x;
+	ElementRect.Top = y;
+	ElementRect.Height = 63;
+	ElementRect.Width = 17;
+	this->colorState = colorState;
+	//modeConteiner = {0, 1, 2, 3};
 }
 
 void  OperatingMode::view()
 {
-	drawBorder(2);
-	TGrahics::outText("pp", x + 2, y, 1, "Verdana12");
-	TGrahics::outText("í", x + 3, y + 8, 1, "MSSansSerifBold14");
-	TGrahics::outText("î", x + 3, y + 21, 1, "MSSansSerifBold14");
-	TGrahics::outText("â", x + 3, y + 34, 1, "MSSansSerifBold14");
-	TGrahics::outText("ò", x + 3, y + 47, 1, "MSSansSerifBold14");
-
-	TFillRect outerBorder{ x-1, y, 17, 63 };
-	TGrahics::InvertArea(outerBorder);
+	background();
+	drawBorder();
+	hidingBorder();
+	TGrahics::outText("pp", ElementRect.Left + 2, ElementRect.Top, std::fabs(colorState - 1), "Verdana12");
+	TGrahics::outText("í", ElementRect.Left + 4, ElementRect.Top + 8, std::fabs(colorState - 1), "MSSansSerifBold14");
+	TGrahics::outText("î", ElementRect.Left + 4, ElementRect.Top + 21, std::fabs(colorState - 1), "MSSansSerifBold14");
+	TGrahics::outText("â", ElementRect.Left + 4, ElementRect.Top + 34, std::fabs(colorState - 1), "MSSansSerifBold14");
+	TGrahics::outText("ò", ElementRect.Left + 4, ElementRect.Top + 47, std::fabs(colorState - 1), "MSSansSerifBold14");
+	
+	if (inFocus) {
+		colorState = 1;
+	}
+	else {
+		colorState = 0;
+	}
+	//TFillRect outerBorder{ x-1, y, 17, 63 };
+	//TGrahics::InvertArea(outerBorder);
 	
 }
 
 const u16 OperatingMode::getHeight(void)
 {
-	return u16(height);
+	return u16(ElementRect.Height);
 }
 
-void OperatingMode::drawBorder(unsigned int yPosition) {
-	TFillRect drawB{ x , y + 12 + (yPosition * 13), 14, 10, 1};
+void OperatingMode::background() {
+	TFillRect drawBorder{ ElementRect.Left, ElementRect.Top, ElementRect.Width -1, ElementRect.Height, std::fabs(colorState - 0) };
+	TGrahics::fillRect(drawBorder);
+}
+
+void OperatingMode::drawBorder() {
+	TFillRect drawBorder{ ElementRect.Left + 1, ElementRect.Top + 12 + (yPosition * 13), 13, 10, std::fabs(colorState - 1)};
+	TGrahics::drawBorder(drawBorder);
+}
+void OperatingMode::stateValue(u8 newColor)
+{
+	colorState = (colorState == 0) ? 1 : 0;
+	TFillRect selectionArea{ ElementRect.Left , ElementRect.Top, ElementRect.Width, ElementRect.Height };
+	TGrahics::InvertArea(selectionArea);
+}
+int OperatingMode::getYPosition()
+{
+	return yPosition;
+}
+void OperatingMode::setYPosition(unsigned int newYPosition)
+{
+	yPosition = newYPosition;
+}
+void OperatingMode::hidingBorder() {
+	TFillRect drawB{ ElementRect.Top + 83, ElementRect.Left - 20 + (yPosition * 13), 13, 10, std::fabs(colorState - 0) };
 	TGrahics::drawBorder(drawB);
 }
 
+bool OperatingMode::ProcessMessage(TMessage* m)
+{
+	switch (m->Event) {
+	case (u32)EventSrc::KEYBOARD: {
+		switch (m->p1) {
+		case (u32)KeyCodes::Up:
+			if (inFocus) {
+				if (yPosition > 0) {
+					setYPosition(getYPosition() - 1);
+				}
+				else {
+					yPosition = 3;
+				}
+			}
+			break;
+		case (u32)KeyCodes::Down:
+			if (inFocus) {
+				if (yPosition < 3) {
+					setYPosition(getYPosition() + 1);
+				}
+				else {
+					yPosition = 0;
+				};
+			}
 
+			break;
+		}
+		for (auto& element : List) {
+			element->ProcessMessage(m);
+		}
+		return false;
+	}
+	}
+}
--- MCU/Components/OperatingMode.h
+++ MCU/Components/OperatingMode.h
@@ -3,15 +3,21 @@
 class OperatingMode : public TVisualObject
 {
 private:
-	int height = 63;
-	int width = 17;
-	int x , y;
+	u8 colorState;
+	unsigned int yPosition = 0;
 public:
 
-	OperatingMode(int x, int y);
+	OperatingMode(int x, int y, u8 colorState);
 	void view() override;
 	const u16 getHeight(void);
-	void drawBorder(unsigned int yPosition);
-
+	void background();
+	void drawBorder();
+	void stateValue(u8 newColor);
+	int getYPosition();
+	void setYPosition(unsigned int newYPosition);
+	void hidingBorder();
+	bool ProcessMessage(TMessage* m);
+	std::vector <TVisualObject*> List;
+	std::vector <TVisualObject*> modeConteiner;
 };
 
