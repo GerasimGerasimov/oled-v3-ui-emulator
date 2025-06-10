d00aec7db1ce647d8e99f6a53621fb2b29a805ce
Надежда Бурнос <N.Burnos@Intmash.Company>
2025-06-10 13:50:58 +0700

    Добавлены параметры класса Питания

---
+++ MCU/Pages/PowerSupply/PagePowerSupply.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <PageSensors.h>
+class TPagePowerSupply : public TPageSensors
+{
+public:
+    TPagePowerSupply(std::string Name);
+protected:
+    virtual void fillPageContainer() override;
+};
+
