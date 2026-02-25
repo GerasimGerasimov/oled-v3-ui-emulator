aae42aab7611d44ba1ff94f12c9548b5ec56805a
Loreleiyy <vadim-lorelei@yandex.ru>
2026-02-25 14:19:39 +0700

    Исправлена страница Флаги работы Изменены фильтры для эмулятора

--- MCU/Pages/PageOperationFlags/PageOperationFlags.cpp
+++ MCU/Pages/PageOperationFlags/PageOperationFlags.cpp
@@ -19,9 +19,9 @@ void TPageOperationFlags::fillPageContainer() {
     LabelInit.Rect = { 10, 10, 10, 80 };
     LabelInit.focused = false;
     TagList->AddList({
-        new TTagLine("OutPolarity", "U1/RAM/flash_error/", LabelInit),
-        new TTagLine("Ready", "U1/RAM/SyncFail/", LabelInit),
-        new TTagLine("Run", "U1/RAM/PowerFail/", LabelInit),
-        new TTagLine("Limit", "U1/RAM/PhaseOrderFail/", LabelInit)
+        new TTagLine("OutPolarity", "U1/RAM/OutPolarity/", LabelInit),
+        new TTagLine("Ready", "U1/RAM/Ready/", LabelInit),
+        new TTagLine("Run", "U1/RAM/Run/", LabelInit),
+        new TTagLine("Limit", "U1/RAM/Limit/", LabelInit)
         });
 }
