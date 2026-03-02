35e1763db8da83516de2d312657d499957fd2cf8
Loreleiyy <vadim-lorelei@yandex.ru>
2026-03-02 14:34:18 +0700

    Изменена обработка LedWarnings После изменения параметра возврат на предыдущую страницу

--- MCU/Pages/EditValue/PageEditValue.cpp
+++ MCU/Pages/EditValue/PageEditValue.cpp
@@ -56,6 +56,7 @@ bool TPageEditValue::ProcessMessage(TMessage* m) {
                     return true;
                 case (u32)KeyCodes::ENT:
                     sendValue();
+                    TRouter::setTask({false, TRouter::getBackPage(), nullptr});
                     return true;
                 }
         }
